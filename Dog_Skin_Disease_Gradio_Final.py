5570ebe32b2866ea705906556ebbbee8bc0dd7ff
Tarık KABAK <tarikkabak@gmail.com>
2025-04-28 11:55:58 +0300

    Update Dog_Skin_Disease_Gradio_Final.py
    Image ile ilgili hata varsa dönüşü eklendi.

--- Dog_Skin_Disease_Gradio_Final.py
+++ Dog_Skin_Disease_Gradio_Final.py
@@ -16,12 +16,15 @@ class_names = ['Dermatitis', 'Healthy', 'Mange', 'Ringworm']
 
 # Görseli alıp tahmin yapacak fonksiyon
 def predict_image(img):
+  try:
     img = img.resize((224, 224))  # Modelin beklediği boyut
     img_array = image.img_to_array(img) / 255.0  # Normalize et
     img_array = np.expand_dims(img_array, axis=0)  # Batch boyutu ekle
     predictions = model.predict(img_array)[0]
     prediction_dict = {class_names[i]: float(predictions[i]) for i in range(len(class_names))}
     return prediction_dict
+  except Exception as e:
+    return {"Error": str(e)}
 
 # Gradio arayüzünü başlat
 interface = gr.Interface(
